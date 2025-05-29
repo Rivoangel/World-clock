c37c480cfaa05e233b1ee41957fd5c7810937595
Rivoangel <makaringerivoningo02@gmail.com>
2025-05-29 22:55:39 +0200

    multiple locations

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ setInterval(function () {
   let sydneyElement = document.querySelector("#sydney");
   let sydneyDateElement = sydneyElement.querySelector(".date");
   let sydneyTimeElement = sydneyElement.querySelector(".time");
-  let sydneyTime = moment().tz("Australia/Sydney");
+  let sydneyTime = moment().tz("Africa/Johannesburg");
 
   sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
   sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>]A[</small>]");
@@ -28,3 +28,20 @@ setInterval(function () {
   parisTimeElement.innerHTML = parisTime.format(
     "h:mm:ss [<small>]A[</small>]" );
 }, 1000);
+
+function updateTime(event){
+    let cityTimeZone= event.target.value;
+    let cityName=cityTimeZone.replace("-"," ").split("/")[1];
+    let cityTime= moment().tz(cityTimeZone);
+    let citiesElement= document.querySelector("#cities");
+
+    citiesElement.innerHTML = `
+    <div class="city">
+        <div><h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div></div>
+            <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
+    </div>`
+};
+
+let citiesSelect=document.querySelector("#city");
+citiesSelect.addEventListener("change", updateCity);
